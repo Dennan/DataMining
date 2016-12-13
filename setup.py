3768facb55a481ecf6336cd372afaf0cdd717309
Dennan <dennan.hogue@gmail.com>
2016-12-13 01:49:55 -0600

    Added requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='DataMining',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Data Mining Group 4',
+    author_email='',
+    description=''
+)
