26f6f8796aac663dbad3ce20a8a395c8cd518efb
Nolan-Labelle <nolanhlabelle@gmail.com>
2016-12-17 12:48:00 -0600

    Added the analysis of the Database using sentiment analysis.

---
+++ db_analysis.py
@@ -0,0 +1,40 @@
+from database import Database
+from sentiment_analysis import SentimentAnalysis
+
+def main() :
+    sa = SentimentAnalysis()
+
+    db = Database()
+    trump_comments = db.db.get('2016-10-1-2016-11-1').get("trump")
+    clinton_comments = db.db.get('2016-10-1-2016-11-1').get("clinton")
+
+    trump_score = 0
+    clinton_score = 0
+
+    for comment in trump_comments :
+        print("Testing classifier with sentence :", comment)
+        result = sa.classify(comment)
+        print(result)
+        if result == "positive" :
+            trump_score = trump_score + 1
+        else :
+            trump_score = trump_score - 1
+
+    for comment in clinton_comments :
+        print("Testing classifier with sentence :", comment)
+        result = sa.classify(comment)
+        print(result)
+        if result == "positive" :
+            clinton_score = clinton_score + 1
+        else :
+            clinton_score = clinton_score - 1
+
+    if clinton_score > trump_score :
+        print("Hilary Clinton has greater support than Donald Trump, at ",clinton_score," to ",trump_score,".")
+    elif trump_score > clinton_score :
+        print("Donald Trump has greater support than Hilary Clinton, at ",trump_score," to ",clinton_score,".")
+    else :
+        print("Hilary Clinton and Donald Trump have the same support, at ",clinton_score," and ",trump_score," each.")
+
+if __name__ == "__main__" :
+    main()
